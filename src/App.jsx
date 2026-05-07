10fb6b08c029ddb38b46b2b1f7b5d55589e5922d
EduardoP-lab <eduardo-padilla@outlook.com>
2026-05-06 18:31:48 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+import './normalize.css'
+
+function App() {
+
+  return (
+    <>
+      <h1>Mi web</h1>
+    </>
+  )
+}
+
+export default App
